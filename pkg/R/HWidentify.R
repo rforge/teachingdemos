9156658b2ba9eccd3048f033b2a46d9bc16b52b6
glsnow <glsnow@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-23 01:45:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/teachingdemos@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/HWidentify.R
+++ pkg/R/HWidentify.R
@@ -5,17 +5,17 @@ HWidentify <- function(x,y,label=seq_along(x), xlab=deparse(substitute(x)),
 
 	dx <- grconvertX(x,to='ndc')
 	dy <- grconvertY(y,to='ndc')
-	
+
 	mm <- function(buttons, xx, yy) {
 		d <- (xx-dx)^2 + (yy-dy)^2
 		if ( all( d > .01 ) ){
 			plot(x,y,xlab=xlab,ylab=ylab,...)
 			return()
 		}
-		w <- which.min(d) 
+		w <- which.min(d)
 		plot(x,y,xlab=xlab,ylab=ylab,...)
 		points(x[w],y[w], cex=2, col='red')
-		text(grconvertX(xx,from='ndc'),grconvertY(yy,from='ndc'), 
+		text(grconvertX(xx,from='ndc'),grconvertY(yy,from='ndc'),
 			label[w], col='green', adj=c(0,0))
 		return()
 	}
@@ -25,13 +25,13 @@ HWidentify <- function(x,y,label=seq_along(x), xlab=deparse(substitute(x)),
 		return()
 	}
 
-	getGraphicsEvent('Right Click to exit', onMouseMove = mm, onMouseDown=md) 
+	getGraphicsEvent('Right Click to exit', onMouseMove = mm, onMouseDown=md)
 	invisible()
 }
 
-tmpx <- runif(25)
-tmpy <- rnorm(25)
-HWidentify(tmpx,tmpy,LETTERS[1:25], pch=letters)
+# tmpx <- runif(25)
+# tmpy <- rnorm(25)
+# HWidentify(tmpx,tmpy,LETTERS[1:25], pch=letters)
 
 
 
@@ -41,7 +41,7 @@ HTKidentify <- function(x,y,label=seq_along(x), xlab=deparse(substitute(x)),
 
 	dx <- numeric(0)
 	dy <- numeric(0)
-	
+
 	xx <- yy <- 0
 
 	replot <- function() {
@@ -54,19 +54,19 @@ HTKidentify <- function(x,y,label=seq_along(x), xlab=deparse(substitute(x)),
 			}
 			return()
 		}
-		w <- which.min(d) 
+		w <- which.min(d)
 		plot(x,y,xlab=xlab,ylab=ylab,...)
 		points(x[w],y[w], cex=2, col='red')
-		text(grconvertX(xx,from='ndc'),grconvertY(yy,from='ndc'), 
+		text(grconvertX(xx,from='ndc'),grconvertY(yy,from='ndc'),
 			label[w], col='green', adj=c(0,0))
 	}
 
 	tt <- tktoplevel()
-	img <- tkrplott(tt, replot, hscale=1.5, vscale=1.5)
+	img <- tkrplot(tt, replot, hscale=1.5, vscale=1.5)
 	tkpack(img, side='top')
 	iw <- as.numeric(tcl("image","width", tkcget(img, "-image")))
 	ih <- as.numeric(tcl("image","width", tkcget(img, "-image")))
-	
+
 	cc <- function(x,y) {
 		x <- (as.real(x) -1)/iw
 		y <- 1-(as.real(y)-1)/ih
@@ -85,9 +85,9 @@ HTKidentify <- function(x,y,label=seq_along(x), xlab=deparse(substitute(x)),
 	invisible()
 }
 
-tmpx <- runif(25)
-tmpy <- rnorm(25)
-HTKidentify(tmpx,tmpy,LETTERS[1:25], pch=letters)
+# tmpx <- runif(25)
+# tmpy <- rnorm(25)
+# HTKidentify(tmpx,tmpy,LETTERS[1:25], pch=letters)
 
 
 
@@ -97,7 +97,7 @@ HTKidentify(tmpx,tmpy,LETTERS[1:25], pch=letters)
 
 
 
-getGraphicsEvent( onMouseDown = function(buttons, xx, yy) cat(buttons,'\n'),
-onMouseUp=function(buttons, xx, yy) cat('up: ',buttons,'\n'),
-onMouseMove=function(buttons, xx, yy) cat('move: ',buttons,' x:',xx,' y:',yy,'\n')
-)
+# getGraphicsEvent( onMouseDown = function(buttons, xx, yy) cat(buttons,'\n'),
+#   onMouseUp=function(buttons, xx, yy) cat('up: ',buttons,'\n'),
+#   onMouseMove=function(buttons, xx, yy) cat('move: ',buttons,' x:',xx,' y:',yy,'\n')
+# )
