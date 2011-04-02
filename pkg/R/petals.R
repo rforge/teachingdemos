3c035bc5b14ffa677b53b304f505090de868c722
glsnow <glsnow@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-02 20:51:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/teachingdemos@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/petals.R
@@ -0,0 +1,43 @@
+petals <- function(plot=TRUE, txt=TRUE) {
+    tmpstr <- "	  		  	 	 	   		   	  	  			  			 	   	 		 	  	    	  		   	  				 	 					    	 		 	 		  		   	 	 	   	 	  	  				 	 		  			  				 	 						  				  	 						  				  	 			 		  				  	 						  				  	 				 	  				  	 						  		 		 	 		  	  	 				    	 	   	 	 		 	 		"
+tmpstr2 <- c(
+             "
+ O
+   ","  O
+
+O  ","O
+ O
+  O","O O
+
+O O","O O
+ O
+O O","O O
+O O
+O O")
+    ans <- eval(parse(text=rawToChar(packBits( unlist(strsplit(tmpstr,''))==' '))))
+
+    resp <- TRUE
+    while(resp) {
+        roll <- unlist(dice(1,5,plot.it=plot))
+        if(txt) {
+            cat("\n---\n")
+            cat(tmpstr2[roll], sep='\n---\n')
+            cat("---\n")
+        }
+        petals <- ans(roll)
+        resp <- readline('How many petals around the rose? ')
+        if(nchar(resp)==0) {
+            cat("There were", petals, "petals around the rose\n")
+            resp <- FALSE
+        } else {
+            if(as.numeric(resp)==petals) {
+                cat("correct, there were", petals,"petals around the rose\n", sep=' ')
+            } else {
+                cat("No, there were", petals, "petals around the rose\n", sep=' ')
+            }
+            resp <- TRUE
+        }
+    }
+}
+
+
