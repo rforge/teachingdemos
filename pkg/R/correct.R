185300f9b2a017c5058d28ad9b26bab504501729
glsnow <glsnow@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-16 22:28:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/teachingdemos@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/correct.R
+++ pkg/R/correct.R
@@ -1,23 +1,23 @@
-cor.rect.plot <- function(x,y, corr=TRUE, 
-                    xlab=deparse(substitute(x)), 
-                    ylab=deparse(substitute(y)), 
+cor.rect.plot <- function(x,y, corr=TRUE,
+                    xlab=deparse(substitute(x)),
+                    ylab=deparse(substitute(y)),
                     col=c('#ff000055','#0000ff55'),
                     ... ) {
   xy <- xy.coords(x,y,xlab=xlab, ylab=ylab)
 	xm <- mean(xy$x)
 	ym <- mean(xy$y)
-  
+
   op <- par(mar=c(5,4,4,4))
   on.exit(par(op))
-  
+
   plot(xy$x, xy$y, xlab=xlab, ylab=ylab, pty='s')
 
   xt <- scale(xy$x, scale=corr)
   yt <- scale(xy$y, scale=corr)
-  
+
   xtt <- pretty(xt)
   ytt <- pretty(yt)
-  
+
   xut <- if(corr) {
     xtt * attr(xt, 'scaled:scale') + attr(xt, 'scaled:center')
   } else {
@@ -28,18 +28,18 @@ cor.rect.plot <- function(x,y, corr=TRUE,
   } else {
     ytt + attr(yt, 'scaled:center')
   }
-  
-  axis(3,at=xut, lab=xtt)
-  axis(4,at=yut, lab=ytt)
-  
+
+  axis(3,at=xut, labels=xtt)
+  axis(4,at=yut, labels=ytt)
+
   abline(h=ym)
   abline(v=xm)
-  
-  
+
+
   ord <- order( xt^2+yt^2, decreasing=TRUE )
   w <- xt[ord]*yt[ord] > 0
   rect(xm, ym, xy$x[ord][w], xy$y[ord][w], col= col[1] )
   rect(xm, ym, xy$x[ord][!w], xy$y[ord][!w], col= col[2] )
   points(xy$x,xy$y)
 }
-  
+
