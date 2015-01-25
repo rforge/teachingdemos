0e0f832d10372a918f3b13b4032a54f23dfc31a6
glsnow <glsnow@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-25 22:53:31 +0000

    updated faces2.Rd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/teachingdemos@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/manipulate.cor.examp.R
@@ -0,0 +1,27 @@
+manipulate.cor.examp <- function(n=100, 
+                                 seed) {
+  if(!require(manipulate)) stop("This function depends on the manipulate package within Rstudio")
+  
+  if(!missing(seed)) set.seed(seed)
+  
+  x <- scale(matrix(rnorm(2*n,0,1), ncol=2))
+  x <- x %*% solve( chol( cor(x)))
+  xr <- range(x,-x)
+  
+  replot <- function(r) {
+    if( r >= 1 ) {
+      cmat <- matrix( c(1,0,1,0), 2 )
+    } else if( r <= -1 ) {
+      cmat <- matrix( c(1,0,-1,0), 2 )
+    } else {
+      cmat <- chol( matrix(c(1,r,r,1),2) )
+    }
+    new.x <- x %*% cmat
+    plot(new.x, xlab='x',ylab='y', 
+         xlim=xr,ylim=xr)
+    title(paste("r =", round(r,3)))
+  }
+  
+  manipulate(replot(r),r=slider(-1,1,0,step=0.005))
+  
+}
